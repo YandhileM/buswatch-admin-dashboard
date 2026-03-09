ce5d9d93b711e005a9105a7fe41e6137714d7cb4
Yandhile <yandhilemaluleke@gmail.com>
2026-03-09 20:32:32 +0200

    feat: Add CreateIncident and RouteDetail routes to the routing configuration

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Incidents = React.lazy(() => import('./views/incidents/Incidents'))
+const CreateIncident = React.lazy(() => import('./views/incidents/CreateIncident'))
 const Buses = React.lazy(() => import('./views/buses/Buses'))
 const Routes = React.lazy(() => import('./views/routes/Routes'))
+const RouteDetail = React.lazy(() => import('./views/routes/RouteDetail'))
 const Devices = React.lazy(() => import('./views/devices/Devices'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/incidents', name: 'Incidents', element: Incidents },
+  { path: '/incidents/new', name: 'Create Incident', element: CreateIncident },
   { path: '/buses', name: 'Buses', element: Buses },
   { path: '/routes', name: 'Routes', element: Routes },
+  { path: '/routes/:id', name: 'Route Detail', element: RouteDetail },
   { path: '/devices', name: 'Devices', element: Devices },
 ]
 
